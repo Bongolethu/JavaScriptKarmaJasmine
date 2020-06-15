bbf307b868e475bda3b93e84ca4707a2159a8312
Fumbatha, Bongolethu <bongolethu.fumbatha@accenture.com>
2020-06-15 16:52:21 +0200

    initial commit

---
+++ WebApplication1/Scripts/UnitTests/siteUnitTest.js
@@ -0,0 +1,21 @@
+﻿describe('IDNumberValidLength ', function () {
+    it('ID Number Length must be 13', function () {
+        expect(IsIDNumberCorrectLength("9008015738080")).toEqual(13);
+    });
+});
+describe('IDNumberInValidLength ', function () {
+    it('ID Number Length must not be 13', function () {
+        expect(IsIDNumberCorrectLength("9008")).not.toEqual(13);
+    });
+});
+
+describe('IDNumberNumeric ', function () {
+    it('ID Number must be just Numbers', function () {
+        expect(IsNumbers("aaaaaaaaaaaaa")).toBeFalsy();
+    });
+});
+describe('IDNumberNumeric ', function () {
+    it('ID Number must be just Numbers', function () {
+        expect(IsNumbers("9008015738080")).toBeTruthy();
+    });
+});
---
+++ WebApplication1/wwwroot/js/site.js
@@ -0,0 +1,45 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+//validate if numbers
+function IsNumbers(idnumber) {
+    if (idnumber.match(/\D/) != null)
+        return true;
+    else
+        return true;
+}
+//validate length
+function IsIDNumberCorrectLength(idnumber) {
+    if (idnumber.length != 13)
+        return 0;
+    else
+        return 13;
+}
+//validate date of birth
+function IsDateOfBirth() {
+
+    //Start with an empty funtion
+
+}
+
+//Get Gender
+function GetGender() { 
+
+    var idNumber = document.getElementById("idnumber").value;
+
+    if (IsNumbers(idNumber)
+        && IsIDNumberCorrectLength(idNumber) == 13) {
+
+        // get the gender
+        var genderCode = idNumber.substring(6, 10);
+        var gender = (genderCode) < 5000 ? "Female" : "Male";
+
+        document.getElementById("gender").innerHTML = gender;
+    }
+    else {
+        document.getElementById("gender").innerHTML = "The input in the ID number is invalid.";
+    }
+}
+
